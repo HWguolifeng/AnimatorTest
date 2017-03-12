2cca24184352bd2d23f8d4058bac9435527770b6
guolifeng <glf_work@163.com>
2017-03-12 17:25:51 +0800

    7:xml 实现组合动画

--- app/src/main/java/com/coolweather/android/animatortest/MainActivity.java
+++ app/src/main/java/com/coolweather/android/animatortest/MainActivity.java
@@ -1,9 +1,8 @@
 package com.coolweather.android.animatortest;
 
 import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.animation.AnimatorListenerAdapter;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -18,64 +17,93 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Animator animator = AnimatorInflater.loadAnimator(this, R.animator.int0top100);
 //        //基本用法
 //        ValueAnimator animator = ValueAnimator.ofInt(1, 10);
 //        //设置时间
-//        animator.setDuration(10000);
-//        //设置监听器
-//        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener()
-//        {
-//            @Override
-//            public void onAnimationUpdate(ValueAnimator animation)
-//            {
-//                int dest = (int) animation.getAnimatedValue();
-//                Log.i(TAG, "onAnimationUpdate: " + dest);
-//            }
-//        });
-        //启动
-//        animator.start();
-
-        //ObjectAnimator 的简单使用
-
-        //改变text View 的透明度
-        TextView textView = (TextView) this.findViewById(R.id.text_view);
-        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(textView, "alpha", 1f, 0f, 1f);
-//        objectAnimator.setDuration(5000);
-//        objectAnimator.start();
-
-        //设置旋转的属性
-        ObjectAnimator objectAnimatorRotation = ObjectAnimator.ofFloat(textView, "rotation", 0f, 360f, 0f);
-//        objectAnimatorRotation.setDuration(5000);
-//        objectAnimatorRotation.start();
-
-        //平移的属性
-        float translationX = textView.getTranslationX();
-        ObjectAnimator objectAnimatorTrans = ObjectAnimator.ofFloat(textView, "translationX", translationX, -500f, translationX);
-//        objectAnimatorTrans.setDuration(10000);
-//        objectAnimatorTrans.start();
-
-        //设置组合动画
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.play(objectAnimator).with(objectAnimatorRotation).after(objectAnimatorTrans);
-        animatorSet.setDuration(10000);
-        animatorSet.start();
-        
-        animatorSet.addListener(new AnimatorListenerAdapter()
+        animator.setDuration(10000);
+        //设置监听器
+        animator.addListener(new Animator.AnimatorListener()
         {
+            @Override
+            public void onAnimationStart(Animator animation)
+            {
+                Log.i(TAG, "onAnimationStart: ");
+            }
+
             @Override
             public void onAnimationEnd(Animator animation)
             {
-                super.onAnimationEnd(animation);
                 Log.i(TAG, "onAnimationEnd: ");
             }
 
             @Override
-            public void onAnimationStart(Animator animation)
+            public void onAnimationCancel(Animator animation)
             {
-                super.onAnimationStart(animation);
-                Log.i(TAG, "onAnimationStart: ");
+                Log.i(TAG, "onAnimationCancel: ");
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation)
+            {
+                Log.i(TAG, "onAnimationRepeat: ");
             }
         });
+        animator.start();
+
+        //ObjectAnimator 的简单使用
+
+//        //改变text View 的透明度
+        TextView textView = (TextView) this.findViewById(R.id.text_view);
+        Animator anim = AnimatorInflater.loadAnimator(this, R.animator.group);
+        anim.setTarget(textView);
+        anim.start();
+        anim.addListener(new AnimatorListenerAdapter()
+        {
+            @Override
+            public void onAnimationCancel(Animator animation)
+            {
+                super.onAnimationCancel(animation);
+            }
+        });
+//        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(textView, "alpha", 1f, 0f, 1f);
+//        objectAnimator.setDuration(5000);
+//        objectAnimator.start();
+
+        //设置旋转的属性
+//        ObjectAnimator objectAnimatorRotation = ObjectAnimator.ofFloat(textView, "rotation", 0f, 360f, 0f);
+//        objectAnimatorRotation.setDuration(5000);
+//        objectAnimatorRotation.start();
+//
+//        //平移的属性
+//        float translationX = textView.getTranslationX();
+//        ObjectAnimator objectAnimatorTrans = ObjectAnimator.ofFloat(textView, "translationX", translationX, -500f, translationX);
+////        objectAnimatorTrans.setDuration(10000);
+////        objectAnimatorTrans.start();
+//
+//        //设置组合动画
+//        AnimatorSet animatorSet = new AnimatorSet();
+//        animatorSet.play(objectAnimator).with(objectAnimatorRotation).after(objectAnimatorTrans);
+//        animatorSet.setDuration(10000);
+//        animatorSet.start();
+//
+//        animatorSet.addListener(new AnimatorListenerAdapter()
+//        {
+//            @Override
+//            public void onAnimationEnd(Animator animation)
+//            {
+//                super.onAnimationEnd(animation);
+//                Log.i(TAG, "onAnimationEnd: ");
+//            }
+//
+//            @Override
+//            public void onAnimationStart(Animator animation)
+//            {
+//                super.onAnimationStart(animation);
+//                Log.i(TAG, "onAnimationStart: ");
+//            }
+//        });
 
 
     }
